db91ea62fcd716616143b173906ef351d69b1768
Pauloslemma <pauloslemma1994@gmail.com>
2023-06-06 12:42:59 -0700

    prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        print("{:d}{:d}".format(i, j), end="")
-        if i == 9 and j == 9:
-            print("")
-        else:
-            print(", ", end="")
+print(", ".join("{:02d}".format(i) for i in range(100)))
