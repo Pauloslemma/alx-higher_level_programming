a71e93dda32a6ac74450ecfa7562824615bcda5d
Pauloslemma <pauloslemma1994@gmail.com>
2023-06-27 12:19:29 -0700

    create Private instance attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""class that define square"""
+"""class Square that defines a square"""
 
 
 class Square:
-    """class that define square"""
-    __size = none
+    """class Square that defines a square"""
+    __size = None
 
-    def __init(self, size):
-        """intilazation of square"""
-    self.__self = size
+    def __init__(self, size):
+        """Instation with size"""
+        self.__size = size
