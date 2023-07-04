ba097834e07bf4b9a8706e69edc238592f50f4b9
Pauloslemma <pauloslemma1994@gmail.com>
2023-07-04 03:52:57 -0700

    All at once

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    iteration = magic_string.iteration + 1 if hasattr(magic_string, 'iteration') else 1
-    magic_string.iteration = iteration
-    return "BestSchool" * iteration
+    magic_string.iteration = magic_string.iteration + 1 if hasattr(magic_string, 'iteration') else 1
+    return "BestSchool" * magic_string.iteration
