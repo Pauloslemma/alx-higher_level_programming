a76e26d8ef76d4c5d1f9edf372891dea396850ca
Pauloslemma <pauloslemma1994@gmail.com>
2023-08-16 12:13:20 -0700

    updated 1-create_user.sql & 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- Connect to the MySQL server as a user with sufficient privileges
-
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
